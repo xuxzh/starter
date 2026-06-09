59a0063f1f5fdd9d4cfc8efd8549b0f4e198cd2c
xuxz <xxz0528@outlook.com>
2026-06-09 10:36:58 +0800

    chore:删除 img-clip 插件

--- lua/plugins/img-clip.lua
+++ lua/plugins/img-clip.lua
@@ -1,24 +1,28 @@
-return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy",
-  opts = {
-    default = {
-      dir = "~/Pictures", -- 默认目录作为后备
-    },
-  },
-  keys = {
-    -- 原有的快捷键不变
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-    -- 新增：粘贴到指定目录（需要提前设置目标目录）
-    {
-      "<leader>P",
-      function()
-        local target_dir = vim.fn.input("Save image to dir: ", vim.fn.getcwd() .. "/", "dir")
-        if target_dir ~= "" then
-          require("img-clip").paste_image({ dir = target_dir })
-        end
-      end,
-      desc = "Paste image to specific directory",
-    },
-  },
-}
+if true then
+  return {}
+end
+
+-- return {
+--   "HakonHarnes/img-clip.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     default = {
+--       dir = "~/Pictures", -- 默认目录作为后备
+--     },
+--   },
+--   keys = {
+--     -- 原有的快捷键不变
+--     { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+--     -- 新增：粘贴到指定目录（需要提前设置目标目录）
+--     {
+--       "<leader>P",
+--       function()
+--         local target_dir = vim.fn.input("Save image to dir: ", vim.fn.getcwd() .. "/", "dir")
+--         if target_dir ~= "" then
+--           require("img-clip").paste_image({ dir = target_dir })
+--         end
+--       end,
+--       desc = "Paste image to specific directory",
+--     },
+--   },
+-- }
